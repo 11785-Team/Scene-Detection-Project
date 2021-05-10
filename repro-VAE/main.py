f79d611f52d3c0fd2e54df961cf490f37d5d7266
dijingzhang <78770000+dijingzhang@users.noreply.github.com>
2021-05-10 02:13:11 -0400

    Add files via upload

--- repro-VAE/main.py
+++ repro-VAE/main.py
@@ -34,15 +34,7 @@ def main():
     Model = BetaVAE(in_channels=3, latent_dim=opts.latent_dim, hidden_dims=opts.hidden_dims, beta=opts.beta,
         gamma=opts.gamma, max_capacity=opts.max_capacity, Capacity_max_iter=opts.Capacity_max_iter, loss_type=opts.loss_type, tau=opts.tau)
 
-    # model_state_path = '/content/gdrive/MyDrive/models_baseline_beta_1/model_state_4_val_loss_443.9217264811198.pkl'
-    # Model.load_state_dict(torch.load(model_state_path))
-    # print("continue training with " + model_state_path)
 
-    # KFold training
-    #K = 5   # split data into K parts
-    #dataset = Dataload(imgpath=img_path, csv_name=csv_name)
-    #kf = KFold(n_splits=K, shuffle=True)
-    #train_loss_sum, val_loss_sum = 0, 0
     train_dataset = Dataload(imgpath=train_path, csv_name=train_csv_name)
     model_state = None
     print("Start Training!!!!!!!")
@@ -50,20 +42,6 @@ def main():
     model_state, train_loss, val_loss = Train(Model, train_dataset, None, batch_size=opts.bs,
     max_iters=opts.max_iters, lr=opts.lr, w_decay=opts.w_decay, m=opts.m, output_folder = opts.output_folder)
         
-
-    torch.save(model_state, 'model_state.pkl')
-    # use trained model to get the latent code of each frame
-    # latent = generate_code(Model, model_state, dataset, opts.latent_dim)
-    # save latent
-    # np.save('latent.npy', latent)
-    
-    # define the distance metric for latent space
-    metric = torch.nn.CrossEntropyLoss
-    index_detected = Detect(latent, dis_metric=eud_dis)
-    print("Frames at the following index experience scene change:")
-    print(sorted(index_detected))
-
-
 if __name__ == '__main__':
     main()
 
